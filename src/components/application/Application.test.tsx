024ef08c8cc1ebe73ca49245b7a109394f368ae7
Mohammed Ali <mdali602@gmail.com>
2023-05-02 20:59:54 +0530

    Added lit staged

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -57,6 +57,6 @@ describe('Application', () => {
 
     const submitButtonElement = screen.getByRole('button')
     expect(submitButtonElement).toBeInTheDocument()
-    expect(submitButtonElement).not.toBeEnabled()
+    expect(submitButtonElement).toBeDisabled()
   })
 })
